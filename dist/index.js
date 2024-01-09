8b8c061cf41398631dc033f5dcf22c99577a2e73
Ayomide24500 <Ayomideadisa83@gmail.com>
2024-01-09 08:22:47 +0100

    production-stage

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = require("dotenv");
+const mainApp_1 = require("./mainApp");
+const dbConfig_1 = require("./utils/dbConfig");
+(0, dotenv_1.config)();
+const port = parseInt(process.env.PORT);
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+(0, mainApp_1.mainApp)(app);
+const server = app.listen(port, () => {
+    console.log("server is running .. 🚀");
+    (0, dbConfig_1.dbConfig)();
+});
+process.on("uncaughtException", (error) => {
+    console.log("uncaughtException", error);
+    process.exit(1);
+});
+process.on("unhandledRejection", (reason) => {
+    console.log("unhandledRejection", reason);
+    server.close(() => {
+        process.exit(1);
+    });
+});
---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, { Application, json } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+
+config();
+
+const port: number = parseInt(process.env.PORT!);
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+const server = app.listen(port, () => {
+  console.log("server is running .. 🚀");
+});
+
+process.on("uncaughtException", (error: Error) => {
+  console.log("uncaughtException", error);
+
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason: Error) => {
+  console.log("unhandledRejection", reason);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
